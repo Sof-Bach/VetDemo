acef13a4ebec0c18bc92620978da8f7e229438c2
Sof-Bach <117988329+Sof-Bach@users.noreply.github.com>
2023-01-30 15:30:24 +0100

    Add files via upload

---
+++ vetDemo/src/main/java/cz/dcos/bachors/vetDemo/VetDemoApplication.java
@@ -0,0 +1,13 @@
+package cz.dcos.bachors.vetDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetDemoApplication.class, args);
+	}
+
+}
---
+++ vetDemo/src/test/java/cz/dcos/bachors/vetDemo/VetDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.dcos.bachors.vetDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
